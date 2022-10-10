02cdecd6e62519f09bbc75dd9a1be8e91a8ab0b4
Taku Shikina <Taku.Shikina@yaskawa.co.jp>
2022-10-10 21:19:32 +0900

    change syntax file type from json to yaml.

--- yamlToJson.bat
+++ yamlToJson.bat
@@ -1,1 +1,4 @@
+npx js-yaml syntaxes/inform.tmLanguage.yaml > syntaxes/inform.tmLanguage.json
 npx js-yaml syntaxes/informdat.tmLanguage.yaml > syntaxes/informdat.tmLanguage.json
+npx js-yaml syntaxes/psc.tmLanguage.yaml > syntaxes/psc.tmLanguage.json
+
